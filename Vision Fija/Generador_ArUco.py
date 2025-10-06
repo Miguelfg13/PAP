b5290e325c3fe0f3188923b9d6cab03d82047c11
im725896 <im725896@iteso.mx>
2025-10-06 09:51:28 -0600

    no message

--- Vision Fija/Generador_ArUco.py
+++ Vision Fija/Generador_ArUco.py
@@ -1,30 +1,22 @@
-import cv2
 import cv2.aruco as aruco
 import matplotlib.pyplot as plt
-import os
-
-# Ruta absoluta del directorio donde está el script
-path_1 = os.path.dirname(os.path.abspath(__file__))
-path_2 = os.path.join(path_1, "Codigos_ArUco")
+import Funciones_Codigos as fc
 
 # Seleccionar el diccionario de marcadores de ArUco
 # DICT_4x4_50, DICT_5x5_100, DICT_6x6_250, DICT_7x7_1000, DICT_ARUCO_ORIGINAL -> 1024
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
-# Genera el marcador con ID # y tamaño ###x### pixeles
-marker_id = [20, 21, 23, 24]
-marker_size = 250
+id_codigos = [20, 21, 23, 24]   # ID de los códigos ArUco
+tamaño_codigo = 250             # tamaño en pixeles
 
-for x in marker_id:
-    marker_image = aruco.generateImageMarker(aruco_dict, x, marker_size)
+for x in id_codigos:
+    imagen_codigo = aruco.generateImageMarker(aruco_dict, x, tamaño_codigo)
 
     # Muestra el marcador con matplotlib
-    plt.imshow(marker_image, cmap='gray')
+    plt.imshow(imagen_codigo, cmap='gray')
     plt.axis('off')
     plt.title(f'ArUco ID {x}')
     plt.show()
 
     # Guardar el marcador como imagen PNG
-    aruco_filename = os.path.join(path_2, f"Codigo_Aruco_{x}.jpg")
-    cv2.imwrite(aruco_filename, marker_image)
-    #cv2.imwirte(f'aruco_{marker_id}.png', marker_image)
+    fc.guardar_Codigo_ArUco(imagen_codigo, x)
